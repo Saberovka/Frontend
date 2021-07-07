67dbe8b2e50bb0dbcea8eefa62f104410062779f
Saberovka <86574729+Saberovka@users.noreply.github.com>
2021-07-07 16:13:19 +0300

    Create ya-bot.js
    Создала бота. Умеет брать ключевую фразу из массива фраз, выводить в поисковую строку и переходить в поисковую выдачу. В выдаче находит ключевой сайт и умеет  переходить на него.

---
+++ ya-bot.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         YA Bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Saberova Nataly
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+let keywords = [
+    "товары и игрушки для детей", 
+    "игрушки с доставкой по Москве", 
+    "интернет магазин детских товаров"
+];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+
+let inputtag = document.getElementsByName("text")[0];
+inputtag.value = keyword;
+
+let btn = document.querySelectorAll("button")[0];
+
+let links = document.links
+console.log("btn= ", btn);
+if (btn !== undefined) {
+    btn.click();
+}
+console.log(links)
+for (let i = 0; i < links.length; i++) {
+    if (links[i].href.includes("detmir.ru")) {
+        let link = links[i];
+        console.log("Найдена строка" + links[i])
+        link.click();
+        break;
+
+    }
+}
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
